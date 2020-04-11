22f606fcac085dc1d270f723495829f24a3a2c9e
Rahul Pandey <hello@rkpandey.com>
2020-04-11 16:51:01 -0700

    Initial commit, simple layout

---
+++ app/src/main/java/edu/stanford/rkpandey/dummyproject/MainActivity.kt
@@ -0,0 +1,15 @@
+package edu.stanford.rkpandey.dummyproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        tvHelloWorld.text = "Updated text!"
+    }
+}
